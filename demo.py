9ab59ff96c7485bb09a3eaf9a45e6a1f26c9c0eb
rodriguezmDNA <joelrome88@gmail.com>
2025-03-09 13:00:38 -0700

    fix pix sort

--- demo.py
+++
@@ -1,8 +0,0 @@
-# demo.py
-import random
-
-# Generate a random number between 1 and 100
-lucky_number = random.randint(1, 100)
-
-# Display the lucky number; PyScript will output this to the page
-print(f"Your lucky random number is: {lucky_number}")
---
+++ image_upload.py
@@ -0,0 +1,20 @@
+
+from js import document, console, window
+from pyodide import create_proxy
+import asyncio
+from pyscript import display
+
+def _upload_file_and_show(e):
+    console.log("Attempted file upload: " + e.target.value)
+    file_list = e.target.files
+    first_item = file_list.item(0)
+
+    new_image = document.createElement('img')
+    new_image.src = window.URL.createObjectURL(first_item)
+    document.getElementById("output_upload").appendChild(new_image)
+
+upload_file = create_proxy(_upload_file_and_show)
+
+document.getElementById("file-upload").addEventListener("change", upload_file)
+
+display(f"uploaded")
